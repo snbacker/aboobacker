e4c613ce4bc5f139360ff8efd546bf076ddb3653
abuback <backernan001@gmail.com>
2020-06-11 22:16:46 +0530

    'new_api'

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -15,7 +15,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "2def70cdfb9efbc99017bc85e0c5e804",
 "/": "2def70cdfb9efbc99017bc85e0c5e804",
-"main.dart.js": "21ebc8bf6835269b939cfb5d620e240a",
+"main.dart.js": "b014e898510adff48045d6dad61e5121",
 "manifest.json": "14e919b7b1dde549ab334b7ee3ce8412"
 };
 
